1899c8bde0a1021ff25cf645bb9c838fd8110982
abduvositAK <anonimgithub@gmail.com>
2022-12-20 22:58:08 +0500

    upload code

--- js/main.js
+++ js/main.js
@@ -6,10 +6,13 @@ var eltime3 = document.querySelector('.time3')
 var eltime4 = document.querySelector('.time4')
 var elMasofa = document.querySelector('.masofa')
 var elBtn = document.querySelector('.btn')
+var elText = document.querySelector('.text')
 
 function masofa(){
     if(elMasofa.value > 0 ){
-        console.log('ishlayapti');
+        
+        elText.textContent = 'Qiziqasiz-mi?'
+
         var time1 = elMasofa.value / 4
         var time2 = elMasofa.value / 20
         var time3 = elMasofa.value / 70
@@ -19,11 +22,22 @@ function masofa(){
         eltime2.textContent = time2.toFixed(2)     
         eltime3.textContent = time3.toFixed(2)
         eltime4.textContent = time4.toFixed(2)
-        
+
+
+
     }
     else{
-        console.log('ok');
+        elText.textContent = "0 dan katta son kiriting! (manfiy emas) "        
     }
 }
 
 
+elMasofa.addEventListener('keyup', function(evn){
+    if(elMasofa.value >= 0){
+        elMasofa.style.border = "2px solid green"
+    }
+    else{
+        elMasofa.style.border = "2px solid red"
+    }
+
+})
